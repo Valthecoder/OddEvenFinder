46181ccbc345fee228e0ee760878f92196b33b74
ValtheCoder <keshia.kdf@gmail.com>
2024-01-25 10:22:16 +0800

    If Function and Odd or even function

--- OddEvenFinder.py
+++ OddEvenFinder.py
@@ -2,4 +2,5 @@
 list1 = [10, 20, 25, 30, 35]
 list2 = [25, 35, 40, 60, 90]
 # If Function and Odd or Even Function
+result_list = [num for num in list1 if num % 2 !=0] + [num for num in list2 if num % 2 ==0]
 # Print result list: [ result ]
