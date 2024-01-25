100c001ab11a872a2549fdbb486ad6b164be42ca
ValtheCoder <keshia.kdf@gmail.com>
2024-01-25 10:23:00 +0800

    Print and Finish

--- OddEvenFinder.py
+++ OddEvenFinder.py
@@ -4,3 +4,4 @@ list2 = [25, 35, 40, 60, 90]
 # If Function and Odd or Even Function
 result_list = [num for num in list1 if num % 2 !=0] + [num for num in list2 if num % 2 ==0]
 # Print result list: [ result ]
+print("result list: ", result_list)
